9a9ae28609131a7825ef2dbfc61a365de595e6a4
HellVolhard07 <itsmevasu84@gmail.com>
2023-01-15 22:15:46 +0530

    Added file history component

---
+++ src/Components/FileTable/FileHistory.tsx
@@ -0,0 +1,82 @@
+import React from 'react'
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Typography,
+} from '@mui/material'
+
+const FileHistory = () => {
+  const empData = [
+    {
+      empId: 123541,
+      name: 'Dhruv Pasricha',
+    },
+    {
+      empId: 541312,
+      name: 'Sarthak Khandelwal',
+    },
+  ]
+  const fileData = {
+    fileNumber: 'GGSIPU/USICT/415724',
+    fileSubject: 'Test file for file.io',
+    createdBy: 123541,
+    currentlyWith: 541312,
+    createdOn: '15/01/2023',
+    status: 1,
+  }
+  return (
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%',
+        height: '100vh',
+      }}
+    >
+      <Card
+        elevation={3}
+        sx={{
+          minWidth: 600,
+          padding: '20px 30px 40px 30px',
+          borderRadius: '20px',
+        }}
+      >
+        <CardContent>
+          <Typography fontWeight='bold' variant='h5' component='div'>
+            {fileData.fileNumber}
+          </Typography>
+          <Typography
+            sx={{ fontSize: 14, mb: 1.5 }}
+            color='text.secondary'
+            gutterBottom
+          >
+            Created on: {fileData.createdOn}
+          </Typography>
+          <Typography sx={{ mb: 1.5 }}>
+            <span style={{ fontWeight: 'bold' }}>Subject: </span>
+            {fileData.fileSubject}
+          </Typography>
+          <Typography variant='body2'>
+            Created by:{' '}
+            {empData.find((emp) => emp.empId === fileData.createdBy)?.name}
+            <br />
+            Currently with:{' '}
+            {empData.find((emp) => emp.empId === fileData.currentlyWith)?.name}
+          </Typography>
+        </CardContent>
+        <br />
+        <CardActions>
+          <Button size='small'>Cancel</Button>
+          <Button variant='contained' size='small'>
+            Mark as received / Assign file
+          </Button>
+        </CardActions>
+      </Card>
+    </div>
+  )
+}
+
+export default FileHistory
--- src/Components/SideBar/SideBar.tsx
+++ src/Components/SideBar/SideBar.tsx
@@ -1,73 +1,123 @@
-import React from 'react';
-import { Box, Drawer, Avatar, List, Divider, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
-import { MoveToInbox, Mail } from '@mui/icons-material';
-import { deepPurple } from '@mui/material/colors';
-import './SideBar.css';
+import React from 'react'
+import {
+  Box,
+  Drawer,
+  Avatar,
+  List,
+  Divider,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from '@mui/material'
+import { MoveToInbox, Mail } from '@mui/icons-material'
+import { deepPurple } from '@mui/material/colors'
+import './SideBar.css'
 
 const SideBar = () => {
-    const drawerWidth = 240;
-    return (
-        <Box sx={{ display: 'flex' }}>
-            <Drawer
-                sx={{
-                    width: drawerWidth,
-                    flexShrink: 0,
-                    '& .MuiDrawer-paper': {
-                        width: drawerWidth,
-                        boxSizing: 'border-box',
-                    },
-                }}
-                variant="permanent"
-                anchor="left"
-            >
-                <Box display="flex" justifyContent="center" alignItems="center" sx={{ pt: 4, pb: 2 }}>
-                    <Avatar sx={{ bgcolor: deepPurple[500], width: 120, height: 120 }}>DP</Avatar>
-                </Box>
+  const drawerWidth = 240
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant='permanent'
+        anchor='left'
+      >
+        <Box
+          display='flex'
+          justifyContent='center'
+          alignItems='center'
+          sx={{ pt: 2, pb: 2 }}
+        >
+          <div style={{ paddingTop: '5px', paddingRight: '10px' }}>
+            <Mail />
+          </div>
 
-                <div
-                    style={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        // marginBottom: '16px',
-                        fontSize: '24px',
-                    }}
-                >
-                    Dhruv Pasricha
-                </div>
-                <div
-                    style={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        marginBottom: '16px',
-                    }}
-                >
-                    Student
-                </div>
-                <Divider />
-                <List>
-                    {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                        <ListItem key={text} disablePadding>
-                            <ListItemButton>
-                                <ListItemIcon>{index % 2 === 0 ? <MoveToInbox /> : <Mail />}</ListItemIcon>
-                                <ListItemText primary={text} />
-                            </ListItemButton>
-                        </ListItem>
-                    ))}
-                </List>
-                <Divider />
-                <List>
-                    {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                        <ListItem key={text} disablePadding>
-                            <ListItemButton>
-                                <ListItemIcon>{index % 2 === 0 ? <MoveToInbox /> : <Mail />}</ListItemIcon>
-                                <ListItemText primary={text} />
-                            </ListItemButton>
-                        </ListItem>
-                    ))}
-                </List>
-            </Drawer>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              // marginBottom: '16px',
+              fontSize: '24px',
+            }}
+          >
+            File.io
+          </div>
         </Box>
-    );
-};
+        <Divider />
+        <Box
+          display='flex'
+          justifyContent='center'
+          alignItems='center'
+          sx={{ pt: 2, pb: 2 }}
+        >
+          <Avatar sx={{ bgcolor: deepPurple[500], width: 120, height: 120 }}>
+            DP
+          </Avatar>
+        </Box>
+        {/* Name */}
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            // marginBottom: '16px',
+            fontSize: '24px',
+          }}
+        >
+          Dhruv Pasricha
+        </div>
+        {/* Role */}
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            marginBottom: '16px',
+          }}
+        >
+          Student
+        </div>
+        <Divider />
+        <List>
+          {[
+            'My Files',
+            'Incoming Files',
+            'Files with me',
+            'Sent',
+            'Track File',
+          ].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <MoveToInbox /> : <Mail />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['Settings'].map((text) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <MoveToInbox />
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </Box>
+  )
+}
 
-export default SideBar;
+export default SideBar
