d5359465efba8ce511803de68ab53e5bc28baf3a
DhruvPasricha <dhruvpasricha0002@gmail.com>
2023-01-15 22:29:06 +0530

    format

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import SideBar from '../src/Components/SideBar/SideBar'
-import NavBar from './Components/NavBar/NavBar'
-// import FileHistory from './Components/FileTable/FileHistory'
-import './App.css'
+import React from 'react';
+import SideBar from '../src/Components/SideBar/SideBar';
+import NavBar from './Components/NavBar/NavBar';
+import './App.css';
 
 const App = () => {
-  return (
-    <div id='app'>
-      <SideBar />
-      <NavBar />
-    </div>
-  )
-}
+    return (
+        <div id="app">
+            <SideBar />
+            <NavBar />
+        </div>
+    );
+};
 
-export default App
+export default App;
--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -1,34 +1,29 @@
-import AddIcon from '@mui/icons-material/Add'
-import { Button } from '@mui/material'
-import React, { useState } from 'react'
-import SearchBox from '../SearchBox/SearchBox'
-import './NavBar.css'
-import AccountMenu from './ProfileIcon/ProfileIcon'
+import AddIcon from '@mui/icons-material/Add';
+import { Button } from '@mui/material';
+import React, { useState } from 'react';
+import SearchBox from '../SearchBox/SearchBox';
+import './NavBar.css';
+import AccountMenu from './ProfileIcon/ProfileIcon';
 
 const NavBar = () => {
-  const [searchValue, setSearchValue] = useState('')
-  return (
-    <div className='navBar'>
-      <div className='navBarTitle'>My Files</div>
-      <div style={{ display: 'flex', gap: '10px' }}>
-        <SearchBox
-          searchValue={searchValue}
-          setSearchValue={setSearchValue}
-          placeholder='Search File'
-          style={{ width: 'unset' }}
-        />
-        <div>
-          <Button className='addFileBtn' variant='outlined' color='primary'>
-            <span style={{ display: 'flex', justifyContent: 'space-between' }}>
-              <AddIcon color='primary' fontSize='small' />
-              <span>Add File</span>
-            </span>
-          </Button>
+    const [searchValue, setSearchValue] = useState('');
+    return (
+        <div className="navBar">
+            <div className="navBarTitle">My Files</div>
+            <div style={{ display: 'flex', gap: '10px' }}>
+                <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} placeholder="Search File" style={{ width: 'unset' }} />
+                <div>
+                    <Button className="addFileBtn" variant="outlined" color="primary">
+                        <span style={{ display: 'flex', justifyContent: 'space-between' }}>
+                            <AddIcon color="primary" fontSize="small" />
+                            <span>Add File</span>
+                        </span>
+                    </Button>
+                </div>
+                <AccountMenu />
+            </div>
         </div>
-        <AccountMenu />
-      </div>
-    </div>
-  )
-}
+    );
+};
 
-export default NavBar
+export default NavBar;
