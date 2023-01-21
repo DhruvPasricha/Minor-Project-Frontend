c4f26ddb7e9558c3e1934660200a8a66fc9d91cb
HellVolhard07 <itsmevasu84@gmail.com>
2023-01-21 18:35:49 +0530

    Add File Table Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import SideBar from '../src/Components/SideBar/SideBar'
 import NavBar from './Components/NavBar/NavBar'
-// import FileHistory from './Components/FileTable/FileHistory'
+import FileHistory from './Components/FileTable/FileHistory'
 import './App.css'
+import FileTable from './Components/FileTable/FileTable'
+import ProfileCard from './Components/ProfileCard/ProfileCard'
+import { Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <div id='app'>
       <SideBar />
       <NavBar />
+      <FileTable />
+      {/* <FileHistory /> */}
+      {/* <ProfileCard /> */}
     </div>
   )
 }
