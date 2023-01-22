806d97f13c69b0c01572839e7617fcceac5235e2
DhruvPasricha <dhruvpasricha0002@gmail.com>
2023-01-22 07:21:04 +0530

    refactor

--- src/Pages/IncomingFiles/IncomingFiles.tsx
+++ src/Pages/IncomingFiles/IncomingFiles.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import DataTable, { Column } from '../../Components/DataTable/DataTable';
 import FILE_STATUS from '../../Components/FileStatus/FileStatus';
-import TableCell from '../../Components/TableCell/TabelCell';
+import TableCell from '../../Components/DataTable/TableCell/TabelCell';
 
 const columns: Column[] = [
     { id: 'fileDetails', label: 'File' },
@@ -16,143 +16,6 @@ const columns: Column[] = [
         format: (value: number) => value.toLocaleString('en-US'),
     },
 ];
-
-interface Data {
-    fileDetails: any;
-    createdDetails: any;
-    receivedDetails: any;
-    status: any;
-}
-
-const datas = [
-    {
-        subject: 'Test subject for File.io',
-        id: 154263,
-        createdby: 'Sarthak Khandelwal',
-        createdon: '20/01/2023',
-        assignedby: 'Dhruv Pasricha',
-        assignedon: '23/01/2023',
-        status: 'IN PROGRESS',
-    },
-    {
-        subject: 'Test subject for File.io',
-        id: 154263,
-        createdby: 'Dhruv Pasricha',
-        createdon: '20/01/2023',
-        assignedby: 'Sarthak Khandelwal',
-        assignedon: '23/01/2023',
-        status: 'IN PROGRESS',
-    },
-    {
-        subject: 'Test subject for File.io',
-        id: 154263,
-        createdby: 'Dhruv Pasricha',
-        createdon: '20/01/2023',
-        assignedby: 'Sarthak Khandelwal',
-        assignedon: '23/01/2023',
-        status: 'IN PROGRESS',
-    },
-    {
-        subject: 'Test subject for File.io',
-        id: 154263,
-        createdby: 'Dhruv Pasricha',
-        createdon: '20/01/2023',
-        assignedby: 'Sarthak Khandelwal',
-        assignedon: '23/01/2023',
-        status: 'IN PROGRESS',
-    },
-    {
-        subject: 'Test subject for File.io',
-        id: 154263,
-        createdby: 'Dhruv Pasricha',
-        createdon: '20/01/2023',
-        assignedby: 'Sarthak Khandelwal',
-        assignedon: '23/01/2023',
-        status: 'IN PROGRESS',
-    },
-    {
-        subject: 'Test subject for File.io',
-        id: 154263,
-        createdby: 'Dhruv Pasricha',
-        createdon: '20/01/2023',
-        assignedby: 'Sarthak Khandelwal',
-        assignedon: '23/01/2023',
-        status: 'IN PROGRESS',
-    },
-    {
-        subject: 'Test subject for File.io',
-        id: 154263,
-        createdby: 'Dhruv Pasricha',
-        createdon: '20/01/2023',
-        assignedby: 'Sarthak Khandelwal',
-        assignedon: '23/01/2023',
-        status: 'IN PROGRESS',
-    },
-    {
-        subject: 'Test subject for File.io',
-        id: 154263,
-        createdby: 'Dhruv Pasricha',
-        createdon: '20/01/2023',
-        assignedby: 'Sarthak Khandelwal',
-        assignedon: '23/01/2023',
-        status: 'IN PROGRESS',
-    },
-    {
-        subject: 'Test subject for File.io',
-        id: 154263,
-        createdby: 'Dhruv Pasricha',
-        createdon: '20/01/2023',
-        assignedby: 'Sarthak Khandelwal',
-        assignedon: '23/01/2023',
-        status: 'IN PROGRESS',
-    },
-    {
-        subject: 'Test subject for File.io',
-        id: 154263,
-        createdby: 'Dhruv Pasricha',
-        createdon: '20/01/2023',
-        assignedby: 'Sarthak Khandelwal',
-        assignedon: '23/01/2023',
-        status: 'IN PROGRESS',
-    },
-    {
-        subject: 'Test subject for File.io',
-        id: 154263,
-        createdby: 'Dhruv Pasricha',
-        createdon: '20/01/2023',
-        assignedby: 'Sarthak Khandelwal',
-        assignedon: '23/01/2023',
-        status: 'IN PROGRESS',
-    },
-    {
-        subject: 'Test subject for File.io',
-        id: 154263,
-        createdby: 'Dhruv Pasricha',
-        createdon: '20/01/2023',
-        assignedby: 'Sarthak Khandelwal',
-        assignedon: '23/01/2023',
-        status: 'IN PROGRESS',
-    },
-    {
-        subject: 'Test subject for File.io',
-        id: 154263,
-        createdby: 'Dhruv Pasricha',
-        createdon: '20/01/2023',
-        assignedby: 'Sarthak Khandelwal',
-        assignedon: '23/01/2023',
-        status: 'IN PROGRESS',
-    },
-    {
-        subject: 'Test subject for File.io',
-        id: 154263,
-        createdby: 'Dhruv Pasricha',
-        createdon: '20/01/2023',
-        assignedby: 'Sarthak Khandelwal',
-        assignedon: '23/01/2023',
-        status: 'IN PROGRESS',
-    },
-];
-
 const data = [
     {
         fileDetails: <TableCell topText={<strong>USICT Attendance File</strong>} bottomText="19919199191" showAvatar={true} />,
@@ -203,6 +66,7 @@ const data = [
         status: FILE_STATUS.CLOSED,
     },
 ];
+
 export default function IncomingFiles() {
     return <DataTable columns={columns} rowWiseData={data}></DataTable>;
 }
