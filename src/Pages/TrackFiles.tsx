7bda6a8e40587dbf5b0ea69e9f3331757fe239a6
HellVolhard07 <itsmevasu84@gmail.com>
2023-01-23 21:49:20 +0530

    Fix file status colors and dynamic data for track file page

--- src/Pages/TrackFiles.tsx
+++ src/Pages/TrackFiles.tsx
@@ -1,51 +1,113 @@
-import React from 'react';
-import { Box, Stepper, Step, StepLabel, Typography } from '@mui/material';
-import FILE_STATUS from '../Components/FileStatus/FileStatus';
+import React from 'react'
+import { Box, Stepper, Step, StepLabel, Typography } from '@mui/material'
+import FILE_STATUS from '../Components/FileStatus/FileStatus'
+
+const getFileStepLabel = (data: FileAction) => {
+  const { action, actionDoneBy, actionDoneFor, actionDoneAt } = data
+
+  const message = {
+    CREATED: `Created by ${actionDoneBy} on ${actionDoneAt}`,
+    RECEIVED: `Received by ${actionDoneBy} from ${actionDoneFor}`,
+    DISPATCHED: `Assigned by ${actionDoneBy} to ${actionDoneFor}`,
+    CLOSED: `Closed by ${actionDoneBy}`,
+  }
+
+  return (
+    <StepLabel
+      optional={<Typography variant='caption'>on {actionDoneAt}</Typography>}
+    >
+      {message[action]}
+    </StepLabel>
+  )
+}
 
 const TrackFiles = () => {
-    const data: { [key: string]: string } = {
-        subject: 'Test subject for File.io',
-        id: '154263',
-        status: 'IN_PROGRESS',
-    };
-    const getSteps = () => {
-        const steps = [];
-        for (let i = 1; i < 10; i++) {
-            const stepTrack = {
-                createdby: 'Sarthak Khandelwal',
-                createdon: '20/01/2023',
-                assignedby: 'Dhruv Pasricha',
-                assignedon: '23/01/2023',
-                assignedto: 'Sarthak Khandelwal',
-            };
-            steps.push(stepTrack);
-        }
-        return steps;
-    };
+  const trackFileData: TrackFileDataResponse = {
+    fileId: 1,
+    fileSubject: 'fileSubject',
+    fileStatus: 'DISPATCHED',
+    actions: [
+      {
+        action: 'CREATED',
+        actionDoneBy: 'user1',
+        actionDoneFor: null,
+        actionDoneAt: '23/10/2031',
+      },
+      {
+        action: 'DISPATCHED',
+        actionDoneBy: 'user1',
+        actionDoneFor: 'user2',
+        actionDoneAt: '23/10/2031',
+      },
+      {
+        action: 'RECEIVED',
+        actionDoneBy: 'user2',
+        actionDoneFor: 'user1',
+        actionDoneAt: '23/10/2031',
+      },
+      {
+        action: 'DISPATCHED',
+        actionDoneBy: 'user2',
+        actionDoneFor: 'user3',
+        actionDoneAt: '23/10/2031',
+      },
+      {
+        action: 'RECEIVED',
+        actionDoneBy: 'user3',
+        actionDoneFor: 'user2',
+        actionDoneAt: '23/10/2031',
+      },
+      {
+        action: 'CLOSED',
+        actionDoneBy: 'user1',
+        actionDoneFor: null,
+        actionDoneAt: '23/10/2031',
+      },
+    ],
+  }
 
-    const steps = getSteps();
+  const steps = trackFileData.actions
 
-    return steps?.length ? (
-        <Box sx={{ padding: '20px 60px' }}>
-            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                <Typography variant="h3">{data.subject}</Typography> {FILE_STATUS.IN_PROCESS}
-            </div>
+  return steps?.length ? (
+    <Box sx={{ padding: '20px 60px 0 60px' }}>
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Typography variant='h3'>{trackFileData.fileSubject}</Typography>{' '}
+        {FILE_STATUS[trackFileData.fileStatus]}
+      </div>
 
-            <Typography sx={{ paddingLeft: '10px', fontWeight: 'bold', marginBottom: '10px' }}>{data.id}</Typography>
+      <Typography
+        sx={{ paddingLeft: '10px', fontWeight: 'bold', marginBottom: '10px' }}
+      >
+        {trackFileData.fileId}
+      </Typography>
 
-            <Stepper activeStep={-1} orientation="vertical" sx={{ maxHeight: '70vh', overflow: 'scroll' }}>
-                {steps.map((step, index) => (
-                    <Step key={index}>
-                        <StepLabel optional={<Typography variant="caption">on {step.assignedon}</Typography>}>
-                            Assigned by {step.assignedby} to {step.assignedto}
-                        </StepLabel>
-                    </Step>
-                ))}
-            </Stepper>
-        </Box>
-    ) : (
-        <></>
-    );
-};
+      <Stepper
+        activeStep={-1}
+        orientation='vertical'
+        sx={{ maxHeight: '70vh', overflowY: 'auto' }}
+      >
+        {steps.map((step, index) => (
+          <Step key={index}>{getFileStepLabel(step)}</Step>
+        ))}
+      </Stepper>
+    </Box>
+  ) : (
+    <></>
+  )
+}
 
-export default TrackFiles;
+export default TrackFiles
+
+interface FileAction {
+  action: 'CLOSED' | 'DISPATCHED' | 'RECEIVED' | 'CREATED'
+  actionDoneBy: string
+  actionDoneFor: string | null
+  actionDoneAt: string
+}
+
+interface TrackFileDataResponse {
+  fileId: number
+  fileSubject: string
+  fileStatus: 'CLOSED' | 'DISPATCHED' | 'RECEIVED' | 'CREATED'
+  actions: FileAction[]
+}
